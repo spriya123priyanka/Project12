bf773528abd5d54fa9c12caabc8675195e71d239
spriya123priyanka <spriya123priyanka@gmail.com>
2019-12-26 10:28:04 +0530

    Test Message

---
+++ sum.java
@@ -0,0 +1,12 @@
+class{
+	public static void main(String args[])
+	{
+		int a,b,c;
+		a=20;
+		b=40;
+		c=200;
+		c=a+b+c;
+		System.out.println("\n the sum of three values"+c);
+	}
+       }  
+   
